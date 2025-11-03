a61ba4ab29e7915db2bec5a37682194b42682700
Ali-Tamerr <extelx66@gmail.com>
2025-11-03 10:08:01 +0200

    Fix part of responsive design in dashboard page

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function DashboardCard({ title, children }) {
   return (
-    <div className="bg-white rounded-lg h-[380px] w-[350px] p-4 shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+    <div className="bg-white rounded-lg h-[380px] w-[350px] 2xl:w-[450px] border border-[#0a0f3373] p-4 shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
       <h4 className="text-sm font-medium text-[#0a0f33] mb-3">{title}</h4>
       <ul className="space-y-1">
         {children}
