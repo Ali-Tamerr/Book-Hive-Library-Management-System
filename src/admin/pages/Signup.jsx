9e49edc5125de65aaf4290aefb3459dcfd9e83f6
Ali-Tamerr <extelx66@gmail.com>
2025-11-16 23:29:41 +0200

    replace with the new logo

--- src/admin/pages/Signup.jsx
+++ src/admin/pages/Signup.jsx
@@ -48,7 +48,7 @@ function Signup() {
       <div className="flex max-[1080px]:flex-col w-full max-w-[900px] h-[520px] bg-white max-[1080px]:h-full rounded-[10px] overflow-hidden shadow-[0_10px_25px_rgba(0,0,0,0.1)]">
       <div className="w-[55%] max-[1080px]:flex hidden max-[1080px]:w-full max-[1080px]:h-full  [1080px]:flex-1  bg-white  items-center justify-center ">
           <div className="text-center w-[85%] max-w-[400px]">
-          <LogoIcon width="120"/>
+          <LogoIcon width="120" className="text-[#0a0f33]"/>
             <h2 className="text-2xl font-semibold text-[#0a0f33] mb-2">Sign Up</h2>
             <p className="text-gray-700 text-sm mb-6">Please provide your information to sign up.</p>
 
@@ -118,8 +118,8 @@ function Signup() {
           </div>
         </div>
         <div className="w-[45%] max-[1080px]:w-full max-[1080px]:h-full [1080px]:flex-1  bg-[#0a0f33] text-white flex flex-col items-center justify-center p-10 rounded-tr-[60px] rounded-br-[60px] max-[1080px]:rounded-none">
-          <img src="/assets/logo.svg" alt="BookHive Logo" className="w-24 mb-5" />
-          <h1 className="text-3xl text-center mb-5 leading-relaxed">
+        <LogoIcon width="120"/>
+        <h1 className="text-3xl text-center mb-5 leading-relaxed">
             BookHive
             <br />
             <span className="block font-['Caveat',cursive] text-3xl font-medium">Library</span>
