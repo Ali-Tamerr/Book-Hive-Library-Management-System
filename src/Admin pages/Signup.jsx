deafd67e71058b3cb9fe080333933c93a5fa8264
Ali-Tamerr <extelx66@gmail.com>
2025-11-21 14:44:12 +0200

    fix logo location

--- src/Admin pages/Signup.jsx
+++ src/Admin pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signup } from '../services/auth.api';
-import LogoIcon from '../../assets/logo.svg?react'
+import LogoIcon from '../assets/logo.svg?react'
 
 
 function Signup() {
