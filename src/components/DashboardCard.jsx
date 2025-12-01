ac40eb1f55365ecc0656723d4595c956dc6ade5d
Ali-Tamerr <extelx66@gmail.com>
2025-12-01 12:54:25 +0200

    apply border radius to scrollbar

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -4,7 +4,7 @@ function DashboardCard({ title, children }) {
   return (
     <div className="bg-white  flex flex-1  flex-col gap-3 min-h-50 h-full rounded-lg  w-full min-w-[380px] py-8 px-4 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
       <h4 className="text-lg font-semibold self-center text-[#0a0f33]">{title}</h4>
-      <ul className="space-y-1 pt-4 overflow-y-auto">
+      <ul className="space-y-1 pt-4 pr-1 overflow-y-auto">
         {children}
       </ul>
     </div>
