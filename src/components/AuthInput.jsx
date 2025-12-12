8c6675e0efda9a05c7567220ef26bbdc0581fc2b
Ali-Tamerr <extelx66@gmail.com>
2025-12-13 00:26:10 +0200

    add auth popups

--- src/components/AuthInput.jsx
+++ src/components/AuthInput.jsx
@@ -8,7 +8,7 @@ const AuthInput = ({ type, placeholder, value, onChange, name, required = false,
             name={name}
             required={required}
             autoComplete={autoComplete || 'off'}
-            className={`w-full p-5 rounded-xl border-2 border-[#3D3E3E] outline-none focus:border-[#1e255e] text-lg max-[1080px]:text-sm max-[1080px]:p-4 ${className}`}
+            className={`w-full p-5 rounded-xl border-2 border-solid border-[#3D3E3E] bg-white outline-none focus:border-[#1e255e] text-lg max-[1080px]:text-sm max-[1080px]:p-4 ${className}`}
         />
     );
 };
