a9d752cef549b1092e196d1fdea106a0ce437a85
Ali-Tamerr <extelx66@gmail.com>
2026-02-11 14:08:26 +0200

    fix width and height issues in cards

--- src/components/DashboardInfoCard.jsx
+++ src/components/DashboardInfoCard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const DashboardInfoCard = ({ icon, title, value, loading }) => {
   return (
-    <div className="flex h-[126px] w-full max-w-[450px] min-w-0 items-center gap-3.5 rounded-xl bg-white py-2 pr-8 pl-2 max-[1540px]:h-full max-[1540px]:flex-1 max-[1540px]:scale-90 max-[650px]:h-[70px] max-[650px]:max-w-full max-[650px]:scale-100 max-[650px]:gap-2 max-[650px]:py-1 max-[650px]:pr-4 max-[650px]:pl-1.5 dark:bg-[#E8E8E8] [1200px]:px-5 [1200px]:py-5">
+    <div className="flex h-[126px] w-full max-w-[450px] min-w-0 pl-5
+    pr-23 items-center gap-3.5 rounded-xl bg-white py-3 max-[1540px]:pr-8 max-[1540px]:pl-2 max-[1540px]:h-full max-[1540px]:flex-1 max-[1540px]:scale-90 max-[650px]:h-[70px] max-[650px]:max-w-full max-[650px]:scale-100 max-[650px]:gap-2 max-[650px]:py-1 max-[650px]:pr-4 max-[650px]:pl-1.5 dark:bg-[#E8E8E8] [1200px]:px-5 [1200px]:py-5">
       <div className="flex h-22 w-22 shrink-0 items-center justify-center rounded-lg bg-[#A7A7A7]/48 p-5 max-[650px]:h-12 max-[650px]:w-12 max-[650px]:p-2.5 dark:bg-[#C0C0C0]">
         {icon}
       </div>
