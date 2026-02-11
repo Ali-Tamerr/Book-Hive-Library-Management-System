91550344a9c208e2820807af0d7d1c0625eed877
Ali-Tamerr <extelx66@gmail.com>
2026-02-11 14:08:26 +0200

    fix width and height issues in cards

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DashboardCard({ title, children }) {
   return (
-    <div className="flex h-full w-full min-w-[280px] flex-1 flex-col gap-2.5 rounded-[11px] bg-white px-3.5 py-7">
+    <div className="flex h-full w-full min-w-[330px] flex-1 flex-col gap-2.5 rounded-[11px] bg-white px-3.5 py-7">
       <h4 className="self-center text-lg font-medium text-[#0a0f33] dark:text-[#121317]">
         {title}
       </h4>
