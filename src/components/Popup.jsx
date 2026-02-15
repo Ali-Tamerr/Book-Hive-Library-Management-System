9513fb18e4d5eef0e6660b0b457ea04bb5b31a35
Ali-Tamerr <extelx66@gmail.com>
2026-02-15 23:19:42 +0200

    apply dark mode on main popup component

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
-import { X } from 'lucide-react';
+import React from "react";
+import { X } from "lucide-react";
 
-const Popup = ({ show, isOpen, onClose, title, icon, children, maxWidthClass }) => {
+const Popup = ({
+  show,
+  isOpen,
+  onClose,
+  title,
+  icon,
+  children,
+  maxWidthClass,
+}) => {
   const isVisible = show !== undefined ? show : isOpen;
 
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 z-[9999]  flex items-center justify-center bg-black/50 backdrop-blur-sm">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50 backdrop-blur-sm">
       {/* Overlay click to close */}
       <div className="absolute inset-0" onClick={onClose}></div>
 
-      <div className={`relative ${maxWidthClass ? maxWidthClass : 'max-w-2xl'} w-full bg-white p-14 pb-0 rounded-xl shadow-xl max-h-[90vh] flex flex-col`}>
-        <div className="flex items-center justify-between pb-5  shrink-0">
-          <div className="flex items-center gap-4"> 
-            {icon && <div className="text-[#0a0f33] bg-[#D7D7D7] flex justify-center items-center min-w-[60px] min-h-[60px] rounded-lg">{icon}</div>}
-            <h2 className="text-xl font-bold text-[#0a0f33] ">{title}</h2>
+      <div
+        className={`relative ${maxWidthClass ? maxWidthClass : "max-w-2xl"} flex max-h-[90vh] w-full flex-col rounded-xl bg-white p-14 pb-0 shadow-xl dark:border dark:border-[#2C2D33] dark:bg-[#131418]`}
+      >
+        <div className="flex shrink-0 items-center justify-between pb-5">
+          <div className="flex items-center gap-4">
+            {icon && (
+              <div className="flex min-h-[60px] min-w-[60px] items-center justify-center rounded-lg bg-[#D7D7D7] text-[#0a0f33]">
+                {icon}
+              </div>
+            )}
+            <h2 className="text-xl font-bold text-[#0a0f33] dark:text-white">
+              {title}
+            </h2>
           </div>
           <button
             onClick={onClose}
-            className="mr-13 p-1 text-[#525252] hover:text-gray-600 border border-[#525252] transition-colors rounded-md hover:bg-gray-100  cursor-pointer"
+            className="mr-13 cursor-pointer rounded-md border border-[#525252] p-1 text-[#525252] transition-colors hover:bg-gray-100 hover:text-gray-600 dark:border-[#E0E0E0] dark:text-[#E0E0E0] dark:hover:bg-[#2C2D33]"
           >
             <X size={14} />
           </button>
         </div>
-        <div className="w-[100%] h-[1px] bg-black mx-auto"></div>
-        <div className="py-8 overflow-y-auto px-4">
-          {children}
-        </div>
+        <div className="mx-auto h-[1px] w-[100%] bg-black dark:bg-[#2C2D33]"></div>
+        <div className="overflow-y-auto px-4 py-8">{children}</div>
       </div>
     </div>
   );
