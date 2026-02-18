2f7713d49ea3eb7f1ed3ba34855be2ffd1976ef5
Ali-Tamerr <extelx66@gmail.com>
2026-02-19 01:10:20 +0200

    Refine FeedbackPopup design to look like figma

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -11,6 +11,7 @@ const FormInput = ({
   rows,
   autocomplete,
   step,
+  className,
 }) => {
   const commonProps = {
     name,
@@ -19,22 +20,28 @@ const FormInput = ({
     placeholder,
     required,
     autoComplete: autocomplete || "off",
-    className:
-      "w-full h-[50px] px-4 py-3 rounded-xl border-1 border-[#3D3E3E] dark:border-[#2C2D33] bg-white dark:bg-[#1A1B20] text-black dark:text-[#D7D7D7] placeholder-[#727374] dark:placeholder-[#5A5B60] outline-none focus:border-[#1e255e] dark:focus:border-[#D7D7D7] text-[13px]",
+    className: `w-full h-[50px] px-4 py-4 rounded-xl border border-[#3D3E3E] dark:border-[#3D3E3E] bg-white dark:bg-[#121317] text-black dark:text-[#D7D7D7] placeholder-[#727374] dark:placeholder-[#5A5B60] outline-none focus:border-[#1e255e] dark:focus:border-[#D7D7D7] text-[13px] ${className || ""}`,
   };
 
   if (step) {
     commonProps.step = step;
   }
 
+  const inputElement =
+    type === "textarea" ? (
+      <textarea {...commonProps} rows={rows || 3} />
+    ) : (
+      <input type={type} {...commonProps} />
+    );
+
+  if (!label) {
+    return inputElement;
+  }
+
   return (
     <div>
       <label className="block text-sm font-medium">{label}</label>
-      {type === "textarea" ? (
-        <textarea {...commonProps} rows={rows || 3} />
-      ) : (
-        <input type={type} {...commonProps} />
-      )}
+      {inputElement}
     </div>
   );
 };
