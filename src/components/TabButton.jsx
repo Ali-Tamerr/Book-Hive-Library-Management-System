63643634f5b45e937466402fc1e6712976682e4d
Ali-Tamerr <extelx66@gmail.com>
2026-03-03 01:25:37 +0200

    restyle TabButton

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const TabButton = ({ label, isActive, onClick, position = 'middle', className = '' }) => {
-    const getBorderRadius = () => {
-        if (position === 'first') return 'rounded-l-xl';
-        if (position === 'last') return 'rounded-r-xl';
-        if (position === 'only') return 'rounded-xl';
-        return '';
-    };
+const TabButton = ({
+  label,
+  isActive,
+  onClick,
+  position = "middle",
+  className = "",
+}) => {
+  const activeClasses =
+    "bg-[#0b0b3b] border-[#0b0b3b] dark:bg-[#D7D7D7] dark:border-[#D7D7D7] text-white dark:text-black";
+  const inactiveClasses =
+    "bg-transparent text-[#0b1730] dark:text-white border-[#0b1730] dark:border-white";
 
-    return (
-        <button
-            onClick={onClick}
-            className={`px-16 h-10 whitespace-nowrap text-sm font-semibold cursor-pointer ${getBorderRadius()} ${isActive ? 'bg-[#0b0b3b] text-white dark:bg-[#292D32] ' : 'bg-[#E3E3E3] dark:bg-[#D7D7D7] dark:text-[#121317] '
-                } ${className}`}
-        >
-            {label}
-        </button>
-    );
+  // Prevent text from shifting by always keeping a 1px border
+  const baseBorder = `border`;
+  const firstBorder = `${baseBorder} rounded-l-xl `;
+  const middleBorder = ` ${baseBorder}`;
+  const lastBorder = ` ${baseBorder} rounded-r-xl pr-20`;
+
+  const borderClasses =
+    position === "first"
+      ? firstBorder
+      : position === "last"
+        ? lastBorder
+        : middleBorder;
+
+  return (
+    <button
+      onClick={onClick}
+      className={`h-10 cursor-pointer whitespace-nowrap px-16 text-2xl uppercase tracking-widest ${borderClasses} ${isActive ? activeClasses : inactiveClasses} ${className}`}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default TabButton;
