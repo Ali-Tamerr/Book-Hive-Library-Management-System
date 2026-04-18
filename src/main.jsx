205700926f57a623565c15f314a8574366f91ea7
Ali-Tamerr <extelx66@gmail.com>
2026-04-18 22:25:58 +0200

    add vercel/speed-insights/react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -21,7 +21,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient} className="">
       <App />
-      {/* <SpeedInsights /> */}
+      <SpeedInsights />
     </QueryClientProvider>
   </StrictMode>,
 )
